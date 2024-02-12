2e74d528aab7d0bdf5b21ff3c1152c708420e1da
nataliapolakova <2polakova.natalia1@gmail.com>
2024-02-12 12:39:10 +0100

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+console.log("Hello, Welcome to My Portfolio Site")
+function menuToggle() {
+var x = document.getElementById('myNavtoggle');
+if (x.className === 'navtoggle') {
+x.className += ' responsive';
+} else {
+x.className = 'navtoggle';
+}
+}
